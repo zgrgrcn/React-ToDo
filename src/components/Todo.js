82ca503fc316178cd1cee0a3355e32f46db6c453
test name <ozgur.gurcan@pia-team.com>
2022-05-31 20:27:13 +0300

    ToDo Application Started

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+function Todo(props) {
+
+    function deleteHandler() {
+        console.log(`Deleting ${props.title}`);
+    }
+
+
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
